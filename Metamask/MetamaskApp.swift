8d4331708e9016dd034befd93d724ff29da260b5
Spike Deng <spikedeng@icloud.com>
2022-05-18 16:24:38 +0800

    feat: repo init

---
+++ Metamask/MetamaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetamaskApp.swift
+//  Metamask
+//
+//  Created by Spike Deng on 18/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct MetamaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
